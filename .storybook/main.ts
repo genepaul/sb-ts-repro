75e2945f8ee41ca9c93fd0b10b3562eb9c292095
Gene Arch <archgene@johndeere.com>
2023-07-20 12:32:01 -0500

    initial commit

---
+++ .storybook/main.ts
@@ -0,0 +1,26 @@
+import type { StorybookConfig } from '@storybook/react-webpack5';
+const config: StorybookConfig = {
+  stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
+  ],
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {},
+  },
+  docs: {
+    autodocs: 'tag',
+  },
+  typescript: {
+    reactDocgen: 'react-docgen-typescript',
+    reactDocgenTypescriptOptions: {
+      propFilter: (prop) => {
+        return prop.parent ? !(/node_modules/.test(prop.parent.fileName)) : true;
+      }
+    }
+  }
+};
+export default config;
